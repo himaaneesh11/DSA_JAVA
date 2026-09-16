e2dbc048d5c934a89369f08addcdaee2d57bb64f
himaaneesh11 <himaaneeshreddy110605@gmail.com>
2026-09-16 16:01:21 +0530

    add stack using linked list

---
+++ src/stacks/stacksusinglinkedlist.java
@@ -0,0 +1,67 @@
+package stacks;
+
+public class stacksusinglinkedlist {
+    Node top;
+    public stacksusinglinkedlist() {
+        this.top = null;
+    }
+
+    public void push(int data) {
+        Node newNode = new Node(data);
+        if (top == null) {
+            top = newNode;
+            return;
+        }
+        newNode.next = top;
+        top = newNode;
+    }
+
+    public int pop() {
+        if (top == null) {
+            System.out.println("Stack is empty");
+            return -1;
+        }
+        int data = top.data;
+        top = top.next;
+        return data;
+    }
+
+
+    public int peek() {
+        if (isEmpty()) {
+            System.out.println("Stack is empty");
+            return -1;
+        }
+        return top.data;
+    }
+
+    public boolean isEmpty() {
+        return top ==null;
+    }
+
+    public void traverse() {
+        if (isEmpty()) {
+            System.out.println("Stack is empty");
+            return;
+        }
+        Node temp = top;
+        while (temp != null) {
+            System.out.print(temp.data + "->");
+            temp = temp.next;
+        }
+        System.out.print("End");
+
+    }
+
+    public static void main(String[] args) {
+        stacksusinglinkedlist stack = new stacksusinglinkedlist();
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+        stack.push(4);
+        stack.push(5);
+        stack.traverse();
+
+
+    }
+}
